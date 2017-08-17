b830d4f2cf4a5b37e7015abee4e97b4843c84455
George Gaylor <GeorgeGaylor83@gmail.com>
2017-08-17 12:48:33 -0700

    Added some test images to the carosel

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,8 +1,20 @@
-$(document).ready(function(){
+var projectScripts = {};
+
+projectScripts.tabs = function(){
   $('header li').on('click', function() {
     var $tabSelection = $(this).data('content');
     $('section').hide();
     $('.' + $tabSelection).fadeIn(50);
   })
   $('header li:first').click();
+}
+
+projectScripts.carosel = function(){
+  $('img').hide();
+  $('img:first').show();
+}
+
+$(document).ready(function(){
+  projectScripts.tabs();
+  projectScripts.carosel();
 })
