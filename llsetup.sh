cac2468164582e57c756719fc8593a282208cd30
George Gaylor <GeorgeGaylor83@gmail.com>
2017-08-24 08:54:21 -0700

    Moved the site to Node

---
+++ llsetup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# script to setup a new linux lab node project
+# the first line denotes this as a bash script file so the OS knows how to handle it
+# sets up node and creates your package.json and other files
+npm init
+# add dependancy and vertion to package.json
+# download the dependancy and put it into node_modules
+npm i --save express
+#npm i --save fs
+# npmi install
+# this is all you need for an existing project
+#npm i
